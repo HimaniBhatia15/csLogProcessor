77c28591d96d53a74956fec0e72ea3bc6a87f882
bhatiah <bhatiah@L10778.int.crawley.gov.uk>
2019-09-05 09:46:55 +0100

    csLogProcessor

--- src/test/java/main/java/com/himani/EventProcessorApplTest.java
+++ src/test/java/main/java/com/himani/EventProcessorApplTest.java
@@ -1,11 +1,7 @@
 package main.java.com.himani;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableAsync
